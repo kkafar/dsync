26f6d1af21bc50fb49229a5000eca16d88d81dbb
Kacper Kafara <kacperkafara@gmail.com>
2025-05-31 18:30:53 +0200

    Fix logger configuration
    In #ce7a35d I've mistakenly configured the PeerService logger in the
    client code...

--- dsync-server/src/logging/mod.rs
+++ dsync-server/src/logging/mod.rs
@@ -2,7 +2,7 @@ use log::LevelFilter;
 use log4rs::{
     Config,
     append::console::ConsoleAppender,
-    config::{Appender, Root},
+    config::{Appender, Logger, Root},
     encode::pattern::PatternEncoder,
 };
 
@@ -15,12 +15,13 @@ pub(crate) fn configure_logging() -> log4rs::Handle {
     let console_appender_name = "stdout";
     let console_appender = ConsoleAppender::builder().encoder(base_encoder).build();
 
-    // let logger = Logger::builder()
-    //     .appender(console_appender_name)
-    //     .build("main", LevelFilter::Trace);
+    let peer_service_logger = Logger::builder()
+        .appender(console_appender_name)
+        .build("pslog", LevelFilter::Trace);
 
     let logging_config = Config::builder()
         .appender(Appender::builder().build(console_appender_name, Box::new(console_appender)))
+        .logger(peer_service_logger)
         .build(
             Root::builder()
                 .appender(console_appender_name)
