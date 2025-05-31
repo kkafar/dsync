57ab47fd1065ec3da081049dd8e1ce872adbba04
Kacper Kafara <kacperkafara@gmail.com>
2025-05-31 15:20:00 +0200

    Expose `port` option in cli args

--- dsync-server/src/cli.rs
+++ dsync-server/src/cli.rs
@@ -12,9 +12,16 @@ pub(crate) struct Args {
         help = "Path to file defining the environment variables. Please note that this will NOT override existing environment variables in case of collisions."
     )]
     pub env_file: Option<PathBuf>,
+
     #[arg(
         long,
         help = "Path to local db. This will overwrite DATABASE_URL env variable if set. Might be necessary to use in case of running the server binary outside of dsync-server workspace dir."
     )]
     pub db_file: Option<PathBuf>,
+
+    #[arg(
+        long,
+        help = "Port number for the server to listen on. This will overwrite SERVER_PORT env variable if set."
+    )]
+    pub port: Option<i32>,
 }
