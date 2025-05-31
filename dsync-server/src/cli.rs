06bfcfdf05d4d5f65a358eaebe0be9e87ba7f068
Kacper Kafara <kacperkafara@gmail.com>
2025-05-31 12:54:23 +0200

    Add help message to `env-file` option

--- dsync-server/src/cli.rs
+++ dsync-server/src/cli.rs
@@ -7,5 +7,9 @@ use clap::Parser;
 #[derive(Parser)]
 #[command(about)]
 pub(crate) struct Args {
+    #[arg(
+        long,
+        help = "Path to file defining the environment variables. Please note that this will NOT override existing environment variables in case of collisions."
+    )]
     pub env_file: Option<PathBuf>,
 }
