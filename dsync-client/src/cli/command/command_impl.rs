649b48ec01b6bb4fcd04f2527c71d80e1dfc202d
Kacper Kafara <kacperkafara@gmail.com>
2025-06-01 10:16:29 +0200

    Rename dsync_proto::client_api to dsync_proto::cli

--- dsync-client/src/cli/command/command_impl.rs
+++ dsync-client/src/cli/command/command_impl.rs
@@ -1,4 +1,4 @@
-use dsync_proto::client_api::{ListHostsRequest, client_api_client::ClientApiClient};
+use dsync_proto::cli::{ListHostsRequest, client_api_client::ClientApiClient};
 
 use super::Commands;
 
--- dsync-proto/src/lib.rs
+++ dsync-proto/src/lib.rs
@@ -1,7 +1,7 @@
 //! This package contains code generated from protobuf specs,
 //! shared between client & server implementations.
 
-pub mod client_api {
+pub mod cli {
     include!("../proto-generated/cli.rs");
 }
 
