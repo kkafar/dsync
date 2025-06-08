6d98ac4a684abebc2caf59710d5c6ce212880ea5
Kacper Kafara <kacperkafara@gmail.com>
2025-06-08 17:27:06 +0200

    Put shared proto defs into separate proto package

--- dsync-proto/src/lib.rs
+++ dsync-proto/src/lib.rs
@@ -1,6 +1,10 @@
 //! This package contains code generated from protobuf specs,
 //! shared between client & server implementations.
 
+pub mod shared {
+    include!("../proto-generated/shared.rs");
+}
+
 pub mod cli {
     include!("../proto-generated/cli.rs");
 }
