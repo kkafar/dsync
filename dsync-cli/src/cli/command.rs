237e56ed0260abfd022348745cde4fcad10b94ed
Kacper Kafara <kacperkafara@gmail.com>
2025-06-08 12:49:55 +0200

    Dispatch commands directly to implementation methods w/o proxy

--- dsync-cli/src/cli/command.rs
+++ dsync-cli/src/cli/command.rs
@@ -25,23 +25,29 @@ impl Commands {
     pub(crate) async fn handle(self) -> anyhow::Result<()> {
         match self {
             Self::Host(subcmd) => match subcmd {
-                host::HostCommand::List { discover } => command::handle_list_hosts().await,
-                host::HostCommand::Discover => command::handle_discover_hosts().await,
+                host::HostCommand::List { discover } => command::host::host_list().await,
+                host::HostCommand::Discover => command::host::host_discover().await,
             },
             Self::File(subcmd) => match subcmd {
-                file::FileCommand::Add { path, group_id } => command::handle_add_file(path).await,
-                file::FileCommand::Remove { path, group_id } => todo!(),
+                file::FileCommand::Add { path, group_id } => command::file::file_add(path).await,
+                file::FileCommand::Remove { path, group_id } => command::file::file_remove().await,
                 file::FileCommand::List {
                     remote_id,
                     group_id,
-                } => command::handle_list_local_files().await,
-                file::FileCommand::Sync => todo!(),
-                file::FileCommand::Unsync => todo!(),
+                } => command::file::file_list(remote_id, group_id).await,
+                file::FileCommand::Sync => command::file::file_sync().await,
+                file::FileCommand::Unsync => command::file::file_unsync().await,
             },
             Self::Group(subcmd) => match subcmd {
-                group::GroupCommand::Create { group_id } => todo!(),
-                group::GroupCommand::Delete { group_id } => todo!(),
-                group::GroupCommand::List { remote_id } => todo!(),
+                group::GroupCommand::Create { group_id } => {
+                    command::group::group_create(group_id).await
+                }
+                group::GroupCommand::Delete { group_id } => {
+                    command::group::group_delete(group_id).await
+                }
+                group::GroupCommand::List { remote_id } => {
+                    command::group::group_list(remote_id).await
+                }
             },
             _ => {
                 log::error!("Unimplemented command!");
--- dsync-cli/src/command.rs
+++ dsync-cli/src/command.rs
@@ -3,23 +3,3 @@ pub(crate) mod group;
 pub(crate) mod host;
 pub(crate) mod model;
 pub(self) mod utils;
-
-use std::path::PathBuf;
-
-pub(super) async fn handle_list_hosts() -> anyhow::Result<()> {
-    host::host_list().await
-}
-
-pub(super) async fn handle_discover_hosts() -> anyhow::Result<()> {
-    host::host_discover().await
-}
-
-// This captures self by reference to avoid problems with dispatching,
-// since the command itself owns the file_path.
-pub(super) async fn handle_add_file(file_path: PathBuf) -> anyhow::Result<()> {
-    file::file_add(file_path).await
-}
-
-pub(super) async fn handle_list_local_files() -> anyhow::Result<()> {
-    file::file_list(None, None).await
-}
