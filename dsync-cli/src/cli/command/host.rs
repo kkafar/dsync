173fabf76512b7d2052ffc2a1e3d1772e38c6407
Kacper Kafara <kacperkafara@gmail.com>
2025-06-08 12:52:52 +0200

    [cli] Add missing docs for host commands (#45)
    Add missing notes for host commands

--- dsync-cli/src/cli/command/host.rs
+++ dsync-cli/src/cli/command/host.rs
@@ -2,10 +2,12 @@ use clap::Subcommand;
 
 #[derive(Subcommand)]
 pub(crate) enum HostCommand {
+    /// List all host known by local deamon.
     List {
         /// Run host discovery before returning the result
         #[arg(short = 'd', long = "discover")]
         discover: Option<bool>,
     },
+    /// First try to discover hosts on local network & then return the list of known hosts.
     Discover,
 }
