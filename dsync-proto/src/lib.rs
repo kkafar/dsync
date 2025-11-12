279583d70787c0a7b7572aa12e0fcc2363efc093
Kacper Kafara <kacperkafara@gmail.com>
2025-11-12 21:30:44 +0100

    Remove commented out code

--- dsync-proto/src/lib.rs
+++ dsync-proto/src/lib.rs
@@ -24,19 +24,3 @@ pub mod model {
         include!("../proto-generated/model.server.rs");
     }
 }
-
-// pub mod shared {
-//     include!("../proto-generated/shared.rs");
-// }
-//
-// pub mod user_agent {
-//     include!("../proto-generated/user_agent.rs");
-// }
-//
-// pub mod server {
-//     include!("../proto-generated/server.rs");
-// }
-//
-// pub mod file_transfer {
-//     include!("../proto-generated/filetransfer.rs");
-// }
